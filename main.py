55cf8d8ab37b8b18ad341cca12f737dc6cae4f53
tianxincsroad <litianxin920@gmail.com>
2022-08-08 20:12:56 -0500

    get and post tested

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from fastapi import fastAPI
-app = fastAPI()
+from fastapi import FastAPI
+from fastapi.params import Body
+
+app = FastAPI()
 
 @app.get("/")
-
 def root():
-    return {"message": "Hello!"}
+    return {"message": "Hello!!!"}
 
+@app.get("/posts")
+def get_posts():
+    return {"data": "posts"}
 
+@app.post("/createposts")
+def create_posts(payload: dict=Body(...)):
+    return {"msg": "created posts"}
